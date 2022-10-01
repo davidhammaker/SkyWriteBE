33d9f256ae0a70f86c918d925e5db6c1f3b7996d
David J. Hammaker <davidjhammaker@gmail.com>
2022-10-01 00:20:58 -0400

    Update storage object admin with a more useful list

--- sky_write_app/admin.py
+++ sky_write_app/admin.py
@@ -6,4 +6,4 @@ from sky_write_app.models import StorageObject
 @admin.register(StorageObject)
 class StorageObjectAdmin(admin.ModelAdmin):
     model = StorageObject
-    list_display = ["name", "user_id", "folder_id", "is_file"]
+    list_display = ["id", "file_uuid", "user_id", "folder_id", "is_file"]
